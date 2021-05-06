a3f04d1b72b53edbfc8fe2dfd281534f01af922f
Mavine Naaman <you@example.com>
2021-05-06 16:20:08 +0300

    working screen

--- screens/Order_delivery/index.js
+++ screens/Order_delivery/index.js
@@ -6,7 +6,7 @@ import {GOOGLE_API_KEY, COLORS, FONTS, SIZES, Icons} from '../../constants';
 import Icon from '../../constants/global/icon';
 import {RedPin} from '../../constants/icons';
 
-const OrderDelivery = ({route, naivigation}) => {
+const OrderDelivery = ({route, navigation}) => {
   const mapView = React.useRef();
 
   const [restaurant, setRestaurant] = React.useState(null);
@@ -67,7 +67,7 @@ const OrderDelivery = ({route, naivigation}) => {
           style={{
             flexDirection: 'row',
             alignItems: 'center',
-            width: SIZES.width * 0.9,
+            width: SIZES.width * 0.8,
             paddingVertical: SIZES.padding,
             paddingHorizontal: SIZES.padding * 2,
             borderRadius: SIZES.radius,
@@ -76,8 +76,8 @@ const OrderDelivery = ({route, naivigation}) => {
           <Image
             source={Icons.RedPin}
             style={{
-              width: 30,
-              height: 30,
+              width: 20,
+              height: 20,
               marginRight: SIZES.padding,
             }}
           />
@@ -87,6 +87,99 @@ const OrderDelivery = ({route, naivigation}) => {
           </View>
 
           <Text style={{...FONTS.body3}}>{Math.ceil(duration)} mins</Text>
+        </View>
+      </View>
+    );
+  }
+
+  function deliveryInfo() {
+    return (
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 50,
+          left: 0,
+          right: 0,
+          height: 50,
+          alignItems: 'center',
+          justifyContent: 'center',
+        }}>
+        <View
+          style={{
+            width: SIZES.width * 0.9,
+            paddingVertical: SIZES.padding * 3,
+            paddingHorizontal: SIZES.padding * 2,
+            borderRadius: SIZES.radius,
+            backgroundColor: COLORS.white,
+          }}>
+          <View style={{flexDirection: 'row', alignItems: 'center'}}>
+            <Image
+              source={restaurant?.courier.avatar}
+              style={{
+                width: 50,
+                height: 50,
+                borderRadius: 25,
+              }}
+            />
+            <View style={{flex: 1, marginLeft: SIZES.padding}}>
+              {/* Name & Rating */}
+              <View
+                style={{flexDirection: 'row', justifyContent: 'space-between'}}>
+                <Text style={{...FONTS.h4}}>{restaurant?.courier.name}</Text>
+
+                <View style={{flexDirection: 'row'}}>
+                  <Image
+                    source={Icons.Star}
+                    {}
+                    style={{
+                      width: 18,
+                      height: 18,
+                      tintColor: COLORS.primary,
+                      marginRight: SIZES.padding,
+                    }}
+                  />
+                  <Text style={{...FONTS.body3}}>{restaurant?.rating}</Text>
+                </View>
+              </View>
+              <Text style={{color: COLORS.darkgray, ...FONTS.body4}}>
+                {restaurant?.name}
+              </Text>
+            </View>
+
+            <View
+              style={{
+                flexDirection: 'row',
+                marginTop: SIZES.padding * 2,
+                justifyContent: 'space-between',
+              }}>
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  height: 50,
+                  marginRight: 10,
+                  backgroundColor: COLORS.primary,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 10,
+                }}
+                onPress={() => navigation.navigate('Home')}>
+                <Text style={{...FONTS.h4, color: COLORS.white}}>Call</Text>
+              </TouchableOpacity>
+
+              <TouchableOpacity
+                style={{
+                  flex: 1,
+                  height: 50,
+                  backgroundColor: COLORS.secondary,
+                  alignItems: 'center',
+                  justifyContent: 'center',
+                  borderRadius: 10,
+                }}
+                onPress={() => navigation.goBack()}>
+                <Text style={{...FONTS.h4, color: COLORS.white}}>Cancel</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
         </View>
       </View>
     );
@@ -207,6 +300,7 @@ const OrderDelivery = ({route, naivigation}) => {
     <View style={{flex: 1}}>
       {map()}
       {destinationHeader()}
+      {deliveryInfo()}
     </View>
   );
 };
